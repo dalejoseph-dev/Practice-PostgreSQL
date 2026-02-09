58e1fa2770da4986ad55cbec51fe2bf2d957523c
dalejoseph-dev <dalejoseph035@gmail.co>
2026-02-09 18:44:57 +0800

    Finished answering all the exercises for the SQL lessons

---
+++ comparing-sql-databases.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER);
+INSERT into users (id, name, age) values (1, 'John Doe', 21);
+INSERT into users (id, name, age) values (2, 1, 33);
+
+SELECT * FROM users;
---
+++ using-sqlite.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (id INTEGER, name TEXT, age INTEGER, is_admin BOOLEAN);
+INSERT INTO users (id, name, age, is_admin) VALUES (1, 'Lane Holland', 27, false);
+INSERT INTO users (id, name, age, is_admin) VALUES (2, 'Allan Rae', 18, true);
+INSERT INTO users (id, name, age, is_admin) VALUES (3, 'Sally Wagoner', 18, true);
+
+SELECT id, name, is_admin FROM users;
