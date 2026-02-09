e55bf151e878d6c5d329db1a83f0370f18368fa4
dalejoseph-dev <dalejoseph035@gmail.co>
2026-02-09 16:54:31 +0800

    solved the problem not able to run sql file

--- Select-Single-Column.sql
+++ Select-Single-Column.sql
@@ -1,4 +1,7 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (id INTEGER, name TEXT, age INTEGER, balance INTEGER, is_admin BOOLEAN);
 INSERT INTO users (id, name, age, balance, is_admin) VALUES (1, 'John Smith', 28, 450.00, true);
 INSERT INTO users (id, name, age, balance, is_admin) VALUES (2, 'Darren Walker', 27, 200.00, true);
 INSERT INTO users (id, name, age, balance, is_admin) VALUES (3, 'Jane Morris', 33, 496.24, false);
+
+SELECT age FROM users;
